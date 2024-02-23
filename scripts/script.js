e856e1cd65ac6d24ffc3e262738b2107919622b9
AniDemi <AniDemi49@gmail.com>
2024-02-23 10:01:46 +0100

    Task 4 part 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
-let slidebtn = document.getElementById("slideshowbtn");
-let slide = document.getElementById("slidelist");
+const slidebtn = document.getElementById("slideshowbtn");
+const slide = document.getElementById("slidelist");
+const square = document.getElementById("square");
 
 function mouseover(button) {
   button.style="background-color:#272727";
@@ -13,4 +14,17 @@ $(slidebtn).click(function() {
   $(slide).slideToggle("fast");
 });
 
+$(square).hover(function() {
+  $(square).animate({
+    width: "150px",
+    height: "150px",
+    opacity: "0.5"
+  }, 1000)
+}, function() {
+  $(square).animate({
+    width: "100px",
+    height: "100px",
+    opacity: "1"
+  })
+});
 
